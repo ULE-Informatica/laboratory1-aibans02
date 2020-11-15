6c085b5d43484eedf251a3dfe0eef7b7080908de
Armando <aibans02@estudiantes.unileon.es>
2020-11-16 00:03:42 +0100

    Finished

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # dps-lab-1
 Laboratory 1
+-----------------------------------------------------------------------------------------------------------------------------
+
+Steps to execute this code:
+	Suposing to be it the route ~/laboratory1-aibans02
+	1. CMake. This will generate all files needed (CMake tool installed its needed)
+		$ cmake ../laboratory1-aibans02/
+	2. Make. This will compile the code and return and executable file called 'P3'
+		$ make
+	3. Execute the code
+		$ ./P3
+
+------------------------------------------------------------------------------------------------------------------------------
+    1. Enumerate those Rules and Recommendation associated that are broken in the previous source code file.
+
+		STR30-C: Do not attempt to modify String literals.
+		STR36-C - STR11-C: Do not specify the bound of a character array initialized with a string literal.
+		MSC34-C: Do not use deprecated or obsolescent functions.
+		STR31-C: Guarantee that storage for strings has sufficient space for character data and the null terminator.
+
+
+    2. Fix any kind of error or mistake. Add a comment explaining your approach for solving it.
+	
+		exampleStrings.c:32:16: warning: ‘return’ with a value, in function returning void
+         		return 1;
+		Sol: As a void function, it must be 'return;'
+
+		exampleStrings.c:51:2: warning: implicit declaration of function ‘gets’; did you mean ‘fgets’? [-Wimplicit-function-declaration]
+  			gets(response);
+		Sol: gets it's an unsafe function, substituted by 'fgets(response, sizeof(response), stdin)'
+
+		exampleStrings.c:73:10: warning: unused variable ‘analitic3’ [-Wunused-variable]
+     			char analitic3[100]="аналитик";
+          		^~~~~~~~~
+		Sol: Unused variable, so it's deleted
+
+		exampleStrings.c:69:9: warning: unused variable ‘size_array2’ [-Wunused-variable]
+     			int size_array2 = 100;
+        		^~~~~~~~~~~
+		Sol: Unused variable, so it's deleted
+
+		exampleStrings.c:68:9: warning: unused variable ‘size_array1’ [-Wunused-variable]
+     			int size_array1 = strlen("аналитик");
+         		^~~~~~~~~~~
+		Sol: Unused variable, so it's deleted
+
+		/tmp/ccs4TTrU.o: En la función `get_y_or_n':
+		exampleStrings.c:(.text+0xd8): aviso: the `gets' function is dangerous and should not be used.
+		Sol: Solved as below, it's the same warning
+
+    3. Compile the file using a software tool for managing the build process using a compiler-independent method (Cmake, scons, etc)
+        Adapt the process for using different compilation tools and parameters (gcc vs g++), -Wall, standards (-std=c99 and -std=c11) ...
+        Compare between different two standards
+
+
+There are several variants. You should choose at least two.
+At the end the source code  should compile without warnings to the variant selected (you can remove/change instructions).
--- exampleStrings.c
+++ exampleStrings.c
@@ -13,95 +13,137 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
- 
-char array1[] = "Foo" "bar";
-char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r', '\0' };
- 
-enum { BUFFER_MAX_SIZE = 1024 };
- 
-const char* s1 = R"foo(
+
+char array1[] = "Foo"
+                "bar";
+char array2[] = {'F', 'o', 'o', 'b', 'a', 'r', '\0'};
+
+enum
+{
+  BUFFER_MAX_SIZE = 1024
+};
+
+const char *s1 = R"foo(
 Hello
 World
 )foo";
-const char* s2 = "\nHello\nWorld\n";
+const char *s2 = "\nHello\nWorld\n";
 
-void gets_example_func(void) {
+void gets_example_func(void)
+{
   char buf[BUFFER_MAX_SIZE];
- 
-  if (fgets(buf, sizeof(buf), stdin) == NULL) {
-        return 1;
+
+  if (fgets(buf, sizeof(buf), stdin) == NULL)
+  {
+    return;
   }
   buf[strlen(buf) - 1] = '\0';
 }
 
-const char *get_dirname(const char *pathname) {
+const char *get_dirname(const char *pathname)
+{
   char *slash;
   slash = strrchr(pathname, '/');
-  if (slash) {
-    *slash = '\0'; /* Undefined behavior */
+  if (slash)
+  {
+    /**
+     * This sentence access to a memory segment not avaliable, so its commented.
+     */
+      
+    // *slash = '\0'; 
   }
   return pathname;
 }
- 
 
-void get_y_or_n(void) {  
-	char response[8];
+void get_y_or_n(void)
+{
+  char response[8];
 
-	printf("Continue? [y] n: ");  
-	gets(response);
+  printf("Continue? [y] n: ");
+  fgets(response, sizeof(response), stdin);
 
-	if (response[0] == 'n') 
-		exit(0);  
+  if (response[0] == 'n')
+    exit(0);
 
-	return;
+  return;
 }
 
- 
 int main(int argc, char *argv[])
 {
-    char key[24];
-    char response[8];
-    char array3[16];
-    char array4[16];
-    char array5 []  = "01234567890123456";
-    char *ptr_char  = "new string literal";
-    int size_array1 = strlen("аналитик");
-    int size_array2 = 100;
-    
-   // char analitic1[size_array1]="аналитик";
-   // char analitic2[size_array2]="аналитик";
-    char analitic3[100]="аналитик";
+  /**
+   * Unused because the code commented below
+   */
 
-    puts(get_dirname(__FILE__));
+  // char key[24];
+  char response[8];
+  char array3[16];
+  /**
+   * Unused because the code commented below
+   */
 
-        
-    strcpy(key, argv[1]);  
-    strcat(key, " = ");  
-    strcat(key, argv[2]);
+  // char array4[16];
 
+  /**
+   * Set but not used warning, could do something to use the variable but I rather prefer
+   * to not add more code. Just notice the warning it's because of this
+   */
+  char array5[] = "01234567890123456";
 
-    fgets(response,sizeof(response),stdin);
-    
-    get_y_or_n();
+  /**
+   * Unused because the code commented below
+   */
 
-    printf ("%s",array1);
-    printf ("\n");
-    printf ("%s",array2);
-    printf ("\n");
- 
-    puts (s1);
-    printf ("\n");
-    puts (s2);
-    printf ("\n");
-    
-    strncpy(array3, array5, sizeof(array3));  
-    strncpy(array4, array3, strlen(array3));
-    
-    array5 [0] = 'M';
-    ptr_char [0] = 'N';
-    
-    array3[sizeof(array3)-1]='\0';
-    
-    
-    return 0;
+  // char *ptr_char = "new string literal";
+
+  /**
+   * Unused variables, so they're commented
+   */
+
+  // char analitic1[size_array1]="аналитик";
+  // char analitic2[size_array2]="аналитик";+
+
+  puts(get_dirname(__FILE__));
+
+  /**
+   * strcpy and strcat are unsafe options that should be replace strcpy_s and strcat_s 
+   * that requires external libraries, so in this case they are commented
+   */
+
+  // strcpy(key, argv[1]);
+  // strcat(key, " = ");
+  // strcat(key, argv[2]);
+
+  fgets(response, sizeof(response), stdin);
+
+  get_y_or_n();
+
+  printf("%s", array1);
+  printf("\n");
+  printf("%s", array2);
+  printf("\n");
+
+  puts(s1);
+  printf("\n");
+  puts(s2);
+  printf("\n");
+
+  /**
+   * As below, strcpy and strcat are unsafe options that should be replace strcpy_s and strcat_s 
+   * that requires external libraries, so in this case they are commented
+   */
+
+  // strncpy(array3, array5, sizeof(array3));
+  // strncpy(array4, array3, strlen(array3));
+
+  array5[0] = 'M';
+
+  /**
+   * Attemp to modify a string literal, commented because is an undefined behavior
+   */
+
+  //ptr_char[0] = 'N';
+
+  array3[sizeof(array3) - 1] = '\0';
+
+  return 0;
 }
